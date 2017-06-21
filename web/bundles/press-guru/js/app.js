2c650bd78cdbfcf1451f932a2a07a01b41a12ae5
Thomas Gigon <gigon.thomas.b@gmail.com>
2017-06-21 18:00:36 +0200

    Responsive design: - Utilisation de bootstrap pour masquer uniquement certains éléments en fonction de la taille de l'écran (.visible-xs-block) - Création de separator pour le menu mobile - Coloration des items de menu mobile en bleu

--- web/bundles/press-guru/js/app.js
+++ web/bundles/press-guru/js/app.js
@@ -22,23 +22,5 @@ angular
             });
 
         $httpProvider.interceptors.push('httpResponseInterceptor');
-    }])
-    .run(function($rootScope) {
-        // Définit une première fois la taille de l'écran dans des variables du rootScope
-        $rootScope.screenWidth = window.innerWidth;
-        $rootScope.screenHeight = window.innerHeight;
-    
-        // Quand la fenêtre se resize, note sa taille dans le rootScope
-        $(window).resize(function() {
-            $rootScope.$apply(function() {
-                $rootScope.screenWidth = window.innerWidth;
-                $rootScope.screenHeight = window.innerHeight;
-            });
-        });
-    
-        // Fonction indiquant si l'écran est de type extra small -> width = <768px
-        $rootScope.isMobileDevice = function() {
-            return ($rootScope.screenWidth < 768) ? true : false;
-        };
-    });
+    }]);
 
